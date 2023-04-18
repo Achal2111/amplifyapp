0106fcbfaa8394159305b5063f0de2b0d01cb98e
Achal2111 <achal.thakor@triosstudent.com>
2023-04-18 19:15:19 -0400

    update

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,6 +3,10 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
+    "aws_appsync_graphqlEndpoint": "https://zwsoxtahezcmpcnztr4uwxr35y.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-t5zbdt75rbdirg4q5my5vfzwsa",
     "aws_cognito_identity_pool_id": "us-east-1:85eabafb-37bc-457f-88fd-61b5d7b485c1",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_vq260yuos",
@@ -26,10 +30,6 @@ const awsmobile = {
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
     ],
-    "aws_appsync_graphqlEndpoint": "https://zwsoxtahezcmpcnztr4uwxr35y.appsync-api.us-east-1.amazonaws.com/graphql",
-    "aws_appsync_region": "us-east-1",
-    "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-t5zbdt75rbdirg4q5my5vfzwsa",
     "aws_user_files_s3_bucket": "amplifyapp9be6252e61094bd89ce22faedcfef41b11000-staging",
     "aws_user_files_s3_bucket_region": "us-east-1"
 };
